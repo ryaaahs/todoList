b6f6219005b04944d82af1556a441f0591e9e95a
Ryaaahs <reily.maahs@gmail.com>
2022-04-12 22:38:48 -0400

    Initial project commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            strawford: [
+              'strawford-regular',
+            ],
+        },
+    },
+    plugins: [],
+}
