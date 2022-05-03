d95e21d4e84d3bc6a2249030762c9c61e211f940
Reily Maahs <reily.maahs@gmail.com>
2022-05-03 18:56:56 -0400

    Add local data saving.

--- todoList.js
+++ todoList.js
@@ -1,33 +1,74 @@
 // GLOBAL VARIABLES
-let itemList = [];
+let itemList = JSON.parse(localStorage.getItem("itemList") || "[]");
+let buttonID = 0;
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    // Add the content to the todoList
+    if (itemList) {
+        recreateTodoList();
+    }
+});
+
+/**
+ * Recreates the the todo list in the DOM by grabbing the contents of the LocalStorage,
+ * and using that information to recreate the items.
+ */
+function recreateTodoList() {
+
+    let todoListElement = document.getElementsByClassName('todo-list-container')[0];
+    
+    for (let i = 0; i < itemList.length; i++) {
+        let itemElement = document.createElement('li');
+        let itemNameElement = document.createElement('span');
+        let completeButtonElement = createCompleteButton();
+        let removeButtonElement = createRemoveButton(); 
+        let item = itemList[i];
+
+        if (item.completeState) {
+            itemElement.classList.add('item-completed');
+            itemNameElement.classList.add('strike-through');
+        }
+
+        itemNameElement.classList.add('todo-list-item-name');
+        itemNameElement.append(document.createTextNode(item.name));
+
+        itemElement.classList.add('todo-list-item');
+        itemElement.append(itemNameElement, completeButtonElement, removeButtonElement);
+        todoListElement.append(itemElement);
+        buttonID++;
+    }
+    
+}
 
 /**
  * Creates a new todo item from the input text.
  * Creating the title, complete button, and remove button.
  */
 function createItem() {
-    let input = document.getElementsByClassName('todo-list-input')[0];
-    let itemValue = input.value; 
+    let inputElement = document.getElementsByClassName('todo-list-input')[0];
+    let itemName = inputElement.value;
 
-    if (itemValue) {
-        let todoList = document.getElementsByClassName('todo-list-container')[0];
-        let item = document.createElement('li');
+    if (itemName) {
+        let todoListElement = document.getElementsByClassName('todo-list-container')[0];
+        let itemElement = document.createElement('li');
+        let itemNameElement = document.createElement('span');
+        let completeButtonElement = createCompleteButton();
+        let removeButtonElement = createRemoveButton(); 
 
-        let itemName = document.createElement('span');
-        itemName.classList.add('todo-list-item-name');
-        itemName.append(document.createTextNode(itemValue))
-
-        let completeButton = createCompleteButton();
-        let removeButton = createRemoveButton(); 
+        itemNameElement.classList.add('todo-list-item-name');
+        itemNameElement.append(document.createTextNode(inputElement.value))
 
         // Remove text from the input
-        input.value = ''; 
+        inputElement.value = ''; 
 
-        item.classList.add('todo-list-item');
-        item.append(itemName, completeButton, removeButton);
+        itemElement.classList.add('todo-list-item');
+        itemElement.append(itemNameElement, completeButtonElement, removeButtonElement);
 
-        todoList.append(item);
-        itemList.push(item);
+        todoListElement.append(itemElement);
+
+        itemList.push({'name': itemName, 'completeState': false});
+        localStorage.setItem('itemList', JSON.stringify(itemList));
+        buttonID++;
     }
 }
 
@@ -37,16 +78,22 @@ function createItem() {
  * @param Number index  The index of the item we're changing
  */
 function completeItem(index) {
+    let todoListElement = document.getElementsByClassName('todo-list-container')[0];
+    let itemElement = todoListElement.getElementsByTagName('li')[index];
+    let itemNameElement = itemElement.getElementsByTagName('span')[0];
     let item = itemList[index];
-    let itemName = item.getElementsByTagName('span')[0];
 
-    if (itemName.classList.contains('strike-through')) {
-        itemName.classList.remove('strike-through');
-        item.classList.remove('item-completed');
+    if (itemNameElement.classList.contains('strike-through')) {
+        itemNameElement.classList.remove('strike-through');
+        itemElement.classList.remove('item-completed');
+        item.completeState = false;
     } else {
-        itemName.classList.add('strike-through');
-        item.classList.add('item-completed');
+        itemNameElement.classList.add('strike-through');
+        itemElement.classList.add('item-completed');
+        item.completeState = true;
     }
+
+    localStorage.setItem('itemList',  JSON.stringify(itemList));
 }
 
 /**
@@ -56,15 +103,22 @@ function completeItem(index) {
  *                      from the todo list
  */
 function removeItem(index) {
-    itemList[index].remove();
+    let todoListElement = document.getElementsByClassName('todo-list-container')[0];
+    let itemElement = todoListElement.getElementsByTagName('li')[index];
+    itemElement.remove();
 
     itemList.splice(index, 1);
 
     for (let i = 0; i < itemList.length; i++) {
-        let buttons = itemList[i].getElementsByTagName('button');
-        buttons[0].id = i;
-        buttons[1].id = i;
+        itemElement = todoListElement.getElementsByTagName('li')[i];
+        let buttonElements = itemElement.getElementsByTagName('button');
+        buttonElements[0].id = i;
+        buttonElements[1].id = i;
+
     }
+
+    buttonID--;
+    localStorage.setItem('itemList', JSON.stringify(itemList));
 }
 
 /**
@@ -73,21 +127,21 @@ function removeItem(index) {
  * @returns Button element with the complete functionality
  */
 function createCompleteButton() {
-    let button = document.createElement('button');
-    let icon = document.createElement('i');
+    let buttonElement = document.createElement('button');
+    let iconElement = document.createElement('i');
     
-    button.classList.add('complete-button');
-    icon.classList.add('fa-solid', 'fa-check');
-    button.append(icon);
-    button.id = itemList.length;
+    buttonElement.classList.add('complete-button');
+    iconElement.classList.add('fa-solid', 'fa-check');
+    buttonElement.append(iconElement);
+    buttonElement.id = buttonID;
 
     // When creating an event, we need to assign a nameless
     // function to the event to call the logic we want
-    button.onclick = function(e) {
+    buttonElement.onclick = function(e) {
         completeItem(this.id);
     }
 
-    return button;
+    return buttonElement;
 }
 
 /**
@@ -96,17 +150,17 @@ function createCompleteButton() {
  * @returns Button element with the remove functionality
  */
 function createRemoveButton() {
-    let button = document.createElement('button');
-    let icon = document.createElement('i');
+    let buttonElement = document.createElement('button');
+    let iconElement = document.createElement('i');
     
-    button.classList.add('remove-button');
-    icon.classList.add('fa-solid', 'fa-xmark');
-    button.append(icon);
-    button.id = itemList.length;
+    buttonElement.classList.add('remove-button');
+    iconElement.classList.add('fa-solid', 'fa-xmark');
+    buttonElement.append(iconElement);
+    buttonElement.id = buttonID;
 
-    button.onclick = function(e) {
+    buttonElement.onclick = function(e) {
         removeItem(this.id);
     }
 
-    return button;
+    return buttonElement;
 }
